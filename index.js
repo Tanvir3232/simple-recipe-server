cb83a5f13002ea9f45db2bfd69914e53acea7a97
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-03 08:57:26 +0600

    send chefs data on server

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
 app.get('/',(req,res) =>{
     res.send('Chef Server is Running');
 });
-
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
 app.listen(port,()=>{
     console.log(`Chef API is runnning on port: ${port}`);
 })
