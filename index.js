9f15b79d695ca812d46843bfad769614fcaa54f0
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-04 11:50:26 +0600

    modify some text

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
 app.get('/',(req,res) =>{
-    res.send('Chef Server is Running');
+    res.send('Recipe Server is Running');
 });
 app.get('/chefs',(req,res)=>{
     res.send(chefs);
